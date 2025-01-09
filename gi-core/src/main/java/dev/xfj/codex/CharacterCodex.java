51248ea694ee41940e915bc4a9261bbfbe0b3f71
Xufuja <xufuja@gmail.com>
2025-01-09 21:40:47 +0100

    Added WeaponCodex record

--- gi-core/src/main/java/dev/xfj/codex/CharacterCodex.java
+++ gi-core/src/main/java/dev/xfj/codex/CharacterCodex.java
@@ -1,16 +1,13 @@
 package dev.xfj.codex;
 
-import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import dev.xfj.database.AvatarData;
 import dev.xfj.database.Database;
 import dev.xfj.jsonschema2pojo.avatarcodexexcelconfigdata.AvatarCodexExcelConfigDataJson;
-import dev.xfj.jsonschema2pojo.avatarexcelconfigdata.AvatarExcelConfigDataJson;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Optional;
 
 public class CharacterCodex {
     private final AvatarCodexExcelConfigDataJson data;
--- gi-core/src/main/java/dev/xfj/codex/Codex.java
+++ gi-core/src/main/java/dev/xfj/codex/Codex.java
@@ -1,7 +1,10 @@
 package dev.xfj.codex;
 
 import dev.xfj.database.AvatarData;
+import dev.xfj.database.Database;
+import dev.xfj.database.WeaponData;
 import dev.xfj.jsonschema2pojo.avatarcodexexcelconfigdata.AvatarCodexExcelConfigDataJson;
+import dev.xfj.jsonschema2pojo.weaponexcelconfigdata.WeaponExcelConfigDataJson;
 
 import java.util.Comparator;
 import java.util.List;
@@ -13,6 +16,14 @@ public class Codex {
                 .stream()
                 .sorted(Comparator.comparing(AvatarCodexExcelConfigDataJson::getSortFactor))
                 .map(CharacterCodex::new)
+                .collect(Collectors.toList());
+    }
+
+    public static List<WeaponCodex> getWeapons() {
+        return WeaponData.getInstance().weaponConfig
+                .stream()
+                .sorted(Comparator.comparing(WeaponExcelConfigDataJson::getId))
+                .map(entry -> new WeaponCodex(entry.getId(), Database.getInstance().getTranslation(entry.getNameTextMapHash())))
                 .collect(Collectors.toList());
     }
 }
