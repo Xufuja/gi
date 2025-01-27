e4f606d7191d5324134e0aa16f4f849ad32c401a
Xufuja <xufuja@gmail.com>
2025-01-27 18:33:53 +0100

    Added new module

---
+++ gi-app/src/test/java/dev/xfj/GiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.xfj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
