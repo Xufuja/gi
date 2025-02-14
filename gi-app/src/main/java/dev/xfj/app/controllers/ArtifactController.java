b98b61265831287d99bf724be51512ca3048816c
Xufuja <xufuja@gmail.com>
2025-02-14 18:32:15 +0100

    Fixed GET /v1/artifacts API call

--- gi-app/src/main/java/dev/xfj/app/controllers/ArtifactController.java
+++ gi-app/src/main/java/dev/xfj/app/controllers/ArtifactController.java
@@ -5,6 +5,7 @@ import dev.xfj.core.services.ArtifactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,12 +13,12 @@ import java.util.List;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
+@RequestMapping("/artifacts")
 public class ArtifactController {
     @Autowired
     private ArtifactService artifactService;
 
     @GetMapping(
-            path = "/v1/artifacts",
             consumes = APPLICATION_JSON_VALUE,
             produces = APPLICATION_JSON_VALUE
     )
--- gi-core/src/main/java/dev/xfj/core/services/ArtifactService.java
+++ gi-core/src/main/java/dev/xfj/core/services/ArtifactService.java
@@ -1,9 +1,9 @@
 package dev.xfj.core.services;
 
-import dev.xfj.core.codex.ArtifactSetCodex;
 import dev.xfj.core.dto.codex.ArtifactEntryDTO;
 import dev.xfj.core.dto.codex.ArtifactSetCodexDTO;
 import dev.xfj.jsonschema2pojo.reliquarycodexexcelconfigdata.ReliquaryCodexExcelConfigDataJson;
+import dev.xfj.jsonschema2pojo.reliquaryexcelconfigdata.ReliquaryExcelConfigDataJson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,20 +25,33 @@ public class ArtifactService {
         return databaseService.reliquaryCodexConfig
                 .stream()
                 .sorted(Comparator.comparing(ReliquaryCodexExcelConfigDataJson::getSortOrder))
-                .map(ArtifactSetCodex::new)
-                .toList()
-                .stream()
-                .map(entry -> new ArtifactSetCodexDTO(
-                        entry.getId(),
-                        entry.getRarity(),
-                        filter(entry.getGoblet()),
-                        filter(entry.getFeather()),
-                        filter(entry.getCirclet()),
-                        filter(entry.getFlower()),
-                        filter(entry.getSands()),
-                        entry.getSortFactor())
+                .map(data -> new ArtifactSetCodexDTO(
+                        data.getSuitId(),
+                        data.getLevel(),
+                        filter(Map.of(data.getCupId(), getName(getArtifact(data.getCupId())))),
+                        filter(Map.of(data.getLeatherId(), getName(getArtifact(data.getLeatherId())))),
+                        filter(Map.of(data.getCapId(), getName(getArtifact(data.getCapId())))),
+                        filter(Map.of(data.getFlowerId(), getName(getArtifact(data.getFlowerId())))),
+                        filter(Map.of(data.getSandId(), getName(getArtifact(data.getSandId())))),
+                        data.getSortOrder())
                 )
                 .collect(Collectors.toList());
+    }
+
+    private ReliquaryExcelConfigDataJson getArtifact(int id) {
+        return databaseService.reliquaryConfig
+                .stream()
+                .filter(artifact -> artifact.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    private String getName(ReliquaryExcelConfigDataJson artifact) {
+        if (artifact != null) {
+            return databaseService.getTranslation(artifact.getNameTextMapHash());
+        } else {
+            return "";
+        }
     }
 
     private ArtifactEntryDTO filter(Map<Integer, String> artifact) {
