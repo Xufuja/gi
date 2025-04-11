137f4ae708ec13a80bb8d3482815bfc9de1de898
Xufuja <xufuja@gmail.com>
2025-04-11 20:51:40 +0200

    Added toString() override

--- gi-generator/src/main/java/dev/xfj/Node.java
+++ gi-generator/src/main/java/dev/xfj/Node.java
@@ -2,7 +2,14 @@ package dev.xfj;
 
 import com.fasterxml.jackson.databind.node.JsonNodeType;
 
+import static java.lang.String.format;
+
 public record Node(
         String path,
         JsonNodeType type
-) {}
+) {
+    @Override
+    public String toString() {
+        return format("%s=%s", path, type.toString());
+    }
+}
