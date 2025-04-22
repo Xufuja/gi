f81dd6dde3282aaed4806e247274886a233303ea
Xufuja <xufuja@gmail.com>
2025-04-22 20:22:06 +0200

    Added all primitive properties to map

--- gi-generator/src/main/java/dev/xfj/OtherGenerator.java
+++ gi-generator/src/main/java/dev/xfj/OtherGenerator.java
@@ -101,6 +101,8 @@ public class OtherGenerator {
 
                             }
                         }
+                    } else {
+                        objects.put(entry, null);
                     }
                 }
             });
