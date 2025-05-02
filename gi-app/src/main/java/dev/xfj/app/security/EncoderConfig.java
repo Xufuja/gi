8c2b0dbf269572908de93206ca19864d19953b34
Xufuja <xufuja@gmail.com>
2025-05-02 21:11:05 +0200

    Add further layers of obfuscation

---
+++ gi-app/src/main/java/dev/xfj/app/security/EncoderConfig.java
@@ -0,0 +1,14 @@
+package dev.xfj.app.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EncoderConfig {
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- gi-app/src/main/java/dev/xfj/app/services/AuthenticationService.java
+++ gi-app/src/main/java/dev/xfj/app/services/AuthenticationService.java
@@ -4,9 +4,11 @@ import dev.xfj.app.entities.Keys;
 import dev.xfj.app.repositories.KeysRepository;
 import dev.xfj.app.security.ApiKeyAuthentication;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Base64;
@@ -15,6 +17,8 @@ import java.util.Optional;
 @Service
 public class AuthenticationService {
     private final KeysRepository keysRepository;
+    @Autowired
+    private PasswordEncoder encoder;
 
     public AuthenticationService(KeysRepository keysRepository) {
         this.keysRepository = keysRepository;
@@ -33,7 +37,15 @@ public class AuthenticationService {
             unauthorized();
         }
 
-        if (!getSecret(apiKey[0]).equals(apiKey[1])) {
+        String decoded = "";
+
+        try {
+            decoded = fromBase64(apiKey[1]);
+        } catch (Exception e) {
+            unauthorized();
+        }
+
+        if (!encoder.matches(decoded, getSecret(apiKey[0]))) {
             unauthorized();
         }
 
@@ -51,10 +63,14 @@ public class AuthenticationService {
             unauthorized();
         }
 
-        return toBase64(key.get().getSecret());
+        return fromBase64(key.get().getSecret());
     }
 
     private String toBase64(String value) {
         return Base64.getEncoder().encodeToString(value.getBytes());
+    }
+
+    private String fromBase64(String value) {
+        return new String(Base64.getDecoder().decode(value));
     }
 }
