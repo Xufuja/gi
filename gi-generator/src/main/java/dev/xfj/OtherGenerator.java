f1852853666c5c0660e925fe077fdbc85cb364b5
Xufuja <xufuja@gmail.com>
2025-05-10 21:16:17 +0200

    Get correct number type

--- gi-generator/src/main/java/dev/xfj/OtherGenerator.java
+++ gi-generator/src/main/java/dev/xfj/OtherGenerator.java
@@ -50,7 +50,7 @@ public class OtherGenerator {
             objects.entrySet().stream()
                     .filter(entry -> entry.getKey().type() == JsonNodeType.ARRAY)
                     .filter(entry -> entry.getValue().size() > 1)
-                    .forEach(entry -> entry.getValue().forEach(object -> System.out.println(format("%s", object.path()))));
+                    .forEach(entry -> entry.getValue().forEach(object -> System.out.println(format("%s=%s", object.path(), object.type() == JsonNodeType.NUMBER ? object.numberType() : object.type()))));
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
